18b56ed4b49c6af0ddf8e7154176224772c22be1
Alex Vi <ife@tut.by>
2022-06-22 21:31:29 +0300

    0.01 - базовый клиент-сервер без мультикаста без статистики просто отправка - приём

---
+++ client/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace UDP_client
+{
+	class Program
+	{
+		static IPAddress remoteAddress;
+
+		static void Main(string[] args)
+		{
+			remoteAddress = IPAddress.Parse("192.168.0.102");
+			
+			try
+            {
+				Thread receiveThread = new Thread(new ThreadStart(RecieveMessages));
+                receiveThread.Start();
+				Console.WriteLine("Client");
+			}
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+		}
+
+		private static void RecieveMessages(){
+			
+			DataProcessor dp = new DataProcessor();
+			
+			UdpClient receiver = new UdpClient(9531);
+			IPEndPoint remoteIp = null;
+            string localAddress = "192.168.0.102";
+
+            try
+            {
+                while (true)
+                {
+                    byte[] data = receiver.Receive(ref remoteIp); // получаем данные
+					dp.PrintData(ref data);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                receiver.Close();
+            }
+		}
+
+		class DataProcessor{
+			public void PrintData(ref byte[] dataArr){
+				ulong timeStamp = BitConverter.ToUInt64(dataArr, 0);
+				double value = BitConverter.ToDouble(dataArr, 8);
+
+				string message = timeStamp.ToString("0000")+"   ---   value = "+value.ToString();
+                Console.WriteLine(message);
+			}
+		}
+	}
+}
---
+++ server/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace udp_server
+{
+	class Program
+	{
+		static IPAddress remoteAddress;
+
+		static void Main(string[] args)
+		{
+			remoteAddress = IPAddress.Parse("192.168.0.102");
+			try
+            {
+				Thread receiveThread = new Thread(new ThreadStart(GenrateMessages));
+                receiveThread.Start();
+				Console.WriteLine("Server");
+			}
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+		}
+
+		private static void GenrateMessages(){
+			byte[] dataArr = new byte[20];
+			int dataSize;
+			DataGenerator generator = new DataGenerator();
+			
+			UdpClient sender = new UdpClient();
+			IPEndPoint endPoint = new IPEndPoint(remoteAddress, 9531);
+			
+			while(true){
+				dataSize = generator.GetData(ref dataArr);
+				sender.Send(dataArr, dataSize, endPoint); 
+				Thread.Sleep(1000);
+			}
+		}
+	}
+
+	class DataGenerator{
+		private const double stepCoef = 1e-4;
+		private double value;
+		private ulong timeStamp;
+		private Random rnd;
+
+		public DataGenerator(){
+			rnd = new Random();
+			value = 6.5;
+			timeStamp = 0;
+		}
+		public int GetData(ref byte[] dataArr){
+			int dataSize=0;
+			byte[] bitData;
+
+			value += (rnd.Next(2000)-1000) * stepCoef;
+			timeStamp++;
+			
+			bitData = BitConverter.GetBytes(timeStamp);
+			Array.Copy(bitData, dataArr, bitData.Length);
+			dataSize += bitData.Length;
+
+			bitData = BitConverter.GetBytes(value);
+			Array.Copy(bitData, 0, dataArr, dataSize, bitData.Length);
+			dataSize += bitData.Length;
+
+			return dataSize;
+		}
+	}
+}
